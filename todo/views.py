e9acdec469fb773c328b3204ad31c3d7a657061e
Chukwuka Peter <peterchukwuka926@gmail.com>
2026-04-11 12:58:03 +0100

    update project files

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
+from .serializers import Todoserializer
+from .model import Todo 
 # Create your views here.
 class TodoAPIView(APIView):
   def get():
